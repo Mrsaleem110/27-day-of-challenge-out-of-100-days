ed8dd12cbd00eb84cf4135f6383cd27cd09a967d
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-05-21 01:41:34 +0500

    adding 27 day of challenge

---
+++ Question 79/app.js
@@ -0,0 +1,8 @@
+// *Question 79:* Creating and Accessing Object Properties: Construct an object representing a car with properties for make, model, and year. Then, show how you can access the model property of the car.
+// *Explain & TIP:* Objects are collections of properties, kind of like a box for storing related information. You can create an object to group related data and access its properties using either dot notation or bracket notation.
+var car = {
+    make: "Toyota",
+    modle: "vigo(dala)",
+    year: 2024
+};
+console.log(car.make);
